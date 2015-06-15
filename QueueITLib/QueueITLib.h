a889641c6084fe8b814660fc55638b5dee8a98cb
Alexander Yaremchuk <queut-it@Alexanders-Mac-mini.local>
2015-06-15 11:00:32 +0200

    Initial Commit

---
+++ QueueITLib/QueueITLib.h
@@ -0,0 +1,13 @@
+//
+//  QueueITLib.h
+//  QueueITLib
+//
+//  Created by Alexander Yaremchuk on 6/15/15.
+//  Copyright (c) 2015 QueueIT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QueueITLib : NSObject
+
+@end
---
+++ QueueITLib/QueueITLib.m
@@ -0,0 +1,13 @@
+//
+//  QueueITLib.m
+//  QueueITLib
+//
+//  Created by Alexander Yaremchuk on 6/15/15.
+//  Copyright (c) 2015 QueueIT. All rights reserved.
+//
+
+#import "QueueITLib.h"
+
+@implementation QueueITLib
+
+@end
